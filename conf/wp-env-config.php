57ca65202d3f1078418a749aca0eb221c2945300
Nicholas <purbeckpixels@gmail.com>
2018-03-15 12:33:06 +0000

    Reflect handpressed/substratum changes.

--- conf/wp-env-config.php
+++ conf/wp-env-config.php
@@ -6,10 +6,26 @@
  */
 
 /**
+ * @var string Directory containing all of the site's files.
+ */
+$root_dir = dirname( __DIR__ );
+
+/**
+ * @var string Document root.
+ */
+$webroot_dir = $root_dir . '/web';
+
+/**
+ * Expose global env() function (see: https://github.com/oscarotero/env).
+ */
+Env::init();
+
+/**
  * Load .env file (see: https://github.com/vlucas/phpdotenv).
  */
-if ( file_exists( dirname( WP_ROOT ) . '/conf/.env' ) ) {
-	$dotenv = new Dotenv\Dotenv( dirname( WP_ROOT ) . '/conf' );
+$dotenv = new Dotenv\Dotenv( $root_dir );
+
+if ( file_exists( $root_dir . '/.env' ) ) {
 	$dotenv->load();
 	$dotenv->required( [ 'WP_HOME', 'DB_NAME', 'DB_USER', 'DB_PASSWORD' ] );
 }
@@ -17,32 +33,32 @@ if ( file_exists( dirname( WP_ROOT ) . '/conf/.env' ) ) {
 /**
  * Database.
  */
-define( 'DB_NAME', getenv( 'DB_NAME' ) );
-define( 'DB_USER', getenv( 'DB_USER' ) );
-define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
-define( 'DB_HOST', getenv( 'DB_HOST' ) ? getenv( 'DATABASE_SERVER' ) : 'localhost' );
-define( 'DB_CHARSET', getenv( 'DB_CHARSET' ) );
-define( 'DB_COLLATE', getenv( 'DB_COLLATE' ) );
+define( 'DB_NAME', env( 'DB_NAME' ) );
+define( 'DB_USER', env( 'DB_USER' ) );
+define( 'DB_PASSWORD', env( 'DB_PASSWORD' ) );
+define( 'DB_HOST', env( 'DB_HOST' ) ? env( 'DATABASE_SERVER' ) : 'localhost' );
+define( 'DB_CHARSET', env( 'DB_CHARSET' ) );
+define( 'DB_COLLATE', env( 'DB_COLLATE' ) );
 
-$table_prefix = getenv( '$table_prefix' ) ?: 'wp_';
+$table_prefix = env( '$table_prefix' ) ?: 'wp_';
 
 /**
  * URLs.
  */
-define( 'WP_HOME', rtrim( getenv( 'WP_HOME' ), '/' ) );
+define( 'WP_HOME', rtrim( env( 'WP_HOME' ), '/' ) );
 define( 'WP_SITEURL', WP_HOME . '/wp' );
 
 /**
  * Custom content folder.
  */
-define( 'WP_CONTENT_FOLDER', '/app' );
-define( 'WP_CONTENT_URL', WP_HOME . WP_CONTENT_FOLDER );
-define( 'WP_CONTENT_DIR', WP_ROOT . WP_CONTENT_FOLDER );
+define( 'CONTENT_DIR', '/app' );
+define( 'WP_CONTENT_URL', WP_HOME . CONTENT_DIR );
+define( 'WP_CONTENT_DIR', $webroot_dir . CONTENT_DIR );
 
 /**
  * Check for https.
  */
-$is_ssl   = (boolean) getenv( 'HTTPS' ) || 443 === getenv( 'SERVER_PORT' ) || 'https' === getenv( 'HTTP_X_FORWARDED_PROTO' );
+$is_ssl   = (boolean) env( 'HTTPS' ) || 443 === env( 'SERVER_PORT' ) || 'https' === env( 'HTTP_X_FORWARDED_PROTO' );
 $protocol = $is_ssl ? 'https' : 'http';
 
 /**
@@ -53,25 +69,25 @@ if ( 'https' === $protocol ) {
 	define( 'FORCE_SSL_ADMIN', true );
 }
 
-define( 'WP_CACHE_KEY_SALT', getenv( 'SERVER_NAME' ) . '_' );
+define( 'WP_CACHE_KEY_SALT', WP_HOME . '_' );
 define( 'FS_CHMOD_DIR', ( 0755 & ~ umask() ) );
 define( 'FS_CHMOD_FILE', ( 0644 & ~ umask() ) );
 define( 'WP_AUTO_UPDATE_CORE', 'minor' );
 
-if ( file_exists( dirname( WP_ROOT ) . '/conf/wp-constants.php' ) ) {
-	require_once dirname( WP_ROOT ) . '/conf/wp-constants.php';
+if ( file_exists( $root_dir . '/conf/wp-constants.php' ) ) {
+	require_once $root_dir . '/conf/wp-constants.php';
 }
 
 /**
  * Authentication unique keys and salts.
  */
-if ( file_exists( dirname( WP_ROOT ) . '/conf/wp-salts.php' ) ) {
-	require_once dirname( WP_ROOT ) . '/conf/wp-salts.php';
+if ( file_exists( $root_dir . '/conf/wp-salts.php' ) ) {
+	require_once $root_dir . '/conf/wp-salts.php';
 }
 
 /**
  * Bootstrap WordPress.
  */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', WP_ROOT . '/wp' );
+	define( 'ABSPATH', $webroot_dir . '/wp' );
 }
